4896126b04b8fdf00f9a05aeae684f8de894b5bd
FeehZera <filipeschneider1@gmail.com>
2025-11-23 19:52:19 -0300

    Fix: data and DDL

--- complex_DDL.sql
+++ complex_DDL.sql
@@ -1,16 +1,16 @@
 
-CREATE SEQUENCE directors_id_director_seq_1;
+CREATE SEQUENCE public.directors_id_director_seq_1;
 
-CREATE TABLE Directors (
-                id_director INTEGER NOT NULL DEFAULT nextval('directors_id_director_seq_1'),
+CREATE TABLE public.Directors (
+                id_director INTEGER NOT NULL DEFAULT nextval('public.directors_id_director_seq_1'),
                 name VARCHAR NOT NULL,
                 CONSTRAINT directors_pk PRIMARY KEY (id_director)
 );
 
 
-ALTER SEQUENCE directors_id_director_seq_1 OWNED BY Directors.id_director;
+ALTER SEQUENCE public.directors_id_director_seq_1 OWNED BY public.Directors.id_director;
 
-CREATE TABLE Movies (
+CREATE TABLE public.Movies (
                 id_movie INTEGER NOT NULL,
                 original_title VARCHAR NOT NULL,
                 overview VARCHAR,
@@ -18,18 +18,19 @@ CREATE TABLE Movies (
                 release_date DATE NOT NULL,
                 status VARCHAR NOT NULL,
                 vote_average NUMERIC,
-                vote_count INTEGER NOT NULL,
+                vote_count INTEGER,
                 cast_1 VARCHAR,
                 original_language VARCHAR,
                 homepage VARCHAR,
                 id_director INTEGER NOT NULL,
+                title VARCHAR NOT NULL,
                 CONSTRAINT movies_pk PRIMARY KEY (id_movie)
 );
 
 
-ALTER TABLE Movies ADD CONSTRAINT directors_movies_fk
+ALTER TABLE public.Movies ADD CONSTRAINT directors_movies_fk
 FOREIGN KEY (id_director)
-REFERENCES Directors (id_director)
+REFERENCES public.Directors (id_director)
 ON DELETE NO ACTION
 ON UPDATE NO ACTION
 NOT DEFERRABLE;
